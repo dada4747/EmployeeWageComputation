61cffa321b8d8ef1e6cd9c974e5ca27f0fb0097b
dada4747 <adsurerahul96@gmail.com>
2021-05-20 12:35:25 +0530

    ADDED: Code to calculate wage of employee

--- src/main/java/com/bridgelabz/empWageComputation.java
+++ src/main/java/com/bridgelabz/empWageComputation.java
@@ -1,13 +1,18 @@
 package com.bridgelabz;
-
+//UC-2-Caclulate daily employee Wage per hrs 20 / 8 hrs per day
 public class empWageComputation {
     public static void main(String args[]){
         final int IS_PRESENT = 1;
+        final int WAGE_PER_HR = 20;
+        int empHrs = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_PRESENT){
-            System.out.println("Employee is Present");
+            empHrs = 8;
         }else{
-            System.out.println("Employee is Absent");
+            empHrs = 0;
         }
+        empWage = WAGE_PER_HR * empHrs;
+        System.out.println("Employee Wage :"+ empWage);
     }
 }
