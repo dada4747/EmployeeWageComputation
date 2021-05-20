393ab68e1f6d5f78dde8db87ee760f3263a35a9f
dada4747 <adsurerahul96@gmail.com>
2021-05-20 13:04:01 +0530

    REFACTORED:Change code to use switch case Statement

--- src/main/java/com/bridgelabz/empWageComputation.java
+++ src/main/java/com/bridgelabz/empWageComputation.java
@@ -1,21 +1,25 @@
 package com.bridgelabz;
-//UC-2-Caclulate daily employee Wage per hrs 20 / 8 hrs per day
+//switch case
 public class empWageComputation {
+    private static final int IS_PART_TIME = 1;
+    private static final int IS_FULL_TIME = 2;
+    private static final int WAGE_PER_HR = 20;
+
     public static void main(String args[]){
-        final int IS_PART_TIME = 1;
-        final int IS_FULL_TIME = 2;
-        final int WAGE_PER_HR = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_PART_TIME){
-            empHrs = 4;
-        }
-        else if (empCheck == IS_FULL_TIME){
-            empHrs = 8;
-        }
-        else {
-            empHrs = 0;
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck){
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+                break;
         }
         empWage = WAGE_PER_HR * empHrs;
         System.out.println("Employee Wage :"+ empWage);
