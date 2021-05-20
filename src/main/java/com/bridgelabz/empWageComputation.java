db68b06fdae48f5f8fc4f0e5bf536e5e35289320
dada4747 <adsurerahul96@gmail.com>
2021-05-20 14:01:53 +0530

    ADDED:Code while condition for EmpWage of month

--- src/main/java/com/bridgelabz/empWageComputation.java
+++ src/main/java/com/bridgelabz/empWageComputation.java
@@ -1,17 +1,21 @@
 package com.bridgelabz;
-//switch case
+
 public class empWageComputation {
     private static final int IS_PART_TIME = 1;
     private static final int IS_FULL_TIME = 2;
     private static final int WAGE_PER_HR = 20;
     private static final int NUM_OF_WORKING_DAYS = 20;
+    private static final int MAX_HRS_IN_MONTH = 100;
 
     public static void main(String args[]){
         int empHrs = 0;
         int empWage = 0;
         int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        for (int day=1; day <= NUM_OF_WORKING_DAYS; day++) {
+        while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_PART_TIME:
@@ -24,10 +28,12 @@ public class empWageComputation {
                     empHrs = 0;
                     break;
             }
-            empWage = WAGE_PER_HR * empHrs;
-            totalEmpWage += empWage;
-            System.out.println("Employee Wage of Day "+ day +" :" + empWage);
+            totalEmpHrs += empHrs;
+            System.out.println("Day :"+totalWorkingDays+"Emp Hrs : "+empHrs);
+
         }
+        totalEmpWage = totalEmpHrs * WAGE_PER_HR;
+        System.out.println("Total Emp Hrs is:"+totalEmpHrs);
         System.out.println("Total Emp Wage is :"+totalEmpWage);
     }
 }
