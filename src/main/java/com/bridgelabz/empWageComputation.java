92b383a8a8d2e4d1ade825ae18dd6550fead562f
dada4747 <adsurerahul96@gmail.com>
2021-05-20 12:49:09 +0530

    ADDED:Code to calculate part time employee wage

--- src/main/java/com/bridgelabz/empWageComputation.java
+++ src/main/java/com/bridgelabz/empWageComputation.java
@@ -2,14 +2,19 @@ package com.bridgelabz;
 //UC-2-Caclulate daily employee Wage per hrs 20 / 8 hrs per day
 public class empWageComputation {
     public static void main(String args[]){
-        final int IS_PRESENT = 1;
+        final int IS_PART_TIME = 1;
+        final int IS_FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PRESENT){
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_PART_TIME){
+            empHrs = 4;
+        }
+        else if (empCheck == IS_FULL_TIME){
             empHrs = 8;
-        }else{
+        }
+        else {
             empHrs = 0;
         }
         empWage = WAGE_PER_HR * empHrs;
