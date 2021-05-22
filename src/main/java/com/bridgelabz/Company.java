28a0ee2db2fdf9b1a65588949e01884b70288471
dada4747 <adsurerahul96@gmail.com>
2021-05-22 22:32:54 +0530

    REFACTORE:code by method,instance,constructor

---
+++ src/main/java/com/bridgelabz/Company.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class Company {
+    public static String company ;
+    public static int wagePerHrs ;
+    public static int numOfWorkinDays ;
+    public static int maxHrsPerMonth ;
+
+    public Company(String company, int wagePerHrs, int numOfWorkinDays, int maxHrsPerMonth) {
+        this.company = company;
+        this.wagePerHrs = wagePerHrs;
+        this.numOfWorkinDays = numOfWorkinDays;
+        this.maxHrsPerMonth = maxHrsPerMonth;
+
+    }
+
+
+}
--- src/main/java/com/bridgelabz/empWageComputation.java
+++ src/main/java/com/bridgelabz/empWageComputation.java
@@ -1,20 +1,19 @@
 package com.bridgelabz;
-
+import java.lang.*;
 public class empWageComputation {
     private static final int IS_PART_TIME = 1;
     private static final int IS_FULL_TIME = 2;
-    private static final int WAGE_PER_HR = 20;
-    private static final int NUM_OF_WORKING_DAYS = 20;
-    private static final int MAX_HRS_IN_MONTH = 100;
 
-    private static void computeEmpWage(int WAGE_PER_HR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
+    public static void computEmpWage( ){
+
         int empHrs = 0;
         int empWage = 0;
         int totalEmpWage = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
 
-        while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        while(totalEmpHrs<=Company.maxHrsPerMonth && totalWorkingDays<Company.numOfWorkinDays) {
+
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -29,18 +28,23 @@ public class empWageComputation {
                     break;
             }
             totalEmpHrs += empHrs;
-            System.out.println("Day :"+totalWorkingDays+"Emp Hrs : "+empHrs);
+            System.out.println("Day : " + totalWorkingDays + " Emp Hrs : " + empHrs);
 
         }
-        totalEmpWage = totalEmpHrs * WAGE_PER_HR;
+
+        totalEmpWage =totalEmpHrs *Company.wagePerHrs;
         System.out.println("Total Emp Hrs is:"+totalEmpHrs);
-        System.out.println("Total Emp Wage is :"+totalEmpWage);
+        System.out.println("Company is: "+Company.company +", Total Emp Wage is :"+totalEmpWage);
+   // return totalEmpWage;
+}
 
-    }
-
-    public static void main(String args[]){
-
-        computeEmpWage(WAGE_PER_HR,NUM_OF_WORKING_DAYS,MAX_HRS_IN_MONTH);
+    public static void main(String[] args){
+        empWageComputation c = new empWageComputation();
+        Company cap = new Company("Capgemini",20,2,10);
+        c.computEmpWage();
+        System.out.println("__________________________________________");
+        Company dmart = new Company("Dmart",20,3,100);
+        c.computEmpWage();
 
     }
 
